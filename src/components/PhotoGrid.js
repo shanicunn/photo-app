fef4ebad82a81b5afc73bbb90e04494adcafce25
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-27 17:26:20 -0400

    Added placeholder photos

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,27 +1,27 @@
 import React from 'react';
-
+import gridPhoto from '../assets/krystal-ng-PrQqQVPzmlw-unsplash.jpg';
 function PhotoGrid() {
     return (
         <div className="photoGrid">
             <div className="row-1">
-                <img className="gridPhoto" src="" alt=""></img>
-                <img className="gridPhoto" src="" alt=""></img>
-                <img className="gridPhoto" src="" alt=""></img>
-                <img className="gridPhoto" src="" alt=""></img>
+                <img className="gridPhoto" src={gridPhoto} alt=""></img>
+                <img className="gridPhoto" src={gridPhoto} alt=""></img>
+                <img className="gridPhoto" src={gridPhoto} alt=""></img>
+                <img className="gridPhoto" src={gridPhoto} alt=""></img>
             </div>
 
             <div className="row-2">
-                <img className="gridPhoto" src="" alt=""></img>
-                <img className="gridPhoto" src="" alt=""></img>
-                <img className="gridPhoto" src="" alt=""></img>
-                <img className="gridPhoto" src="" alt=""></img>
+                <img className="gridPhoto" src={gridPhoto} alt=""></img>
+                <img className="gridPhoto" src={gridPhoto} alt=""></img>
+                <img className="gridPhoto" src={gridPhoto} alt=""></img>
+                <img className="gridPhoto" src={gridPhoto} alt=""></img>
             </div>
 
             <div className="row-3">
-            <img className="gridPhoto" src="" alt=""></img>
-            <img className="gridPhoto" src="" alt=""></img>
-            <img className="gridPhoto" src="" alt=""></img>
-            <img className="gridPhoto" src="" alt=""></img>
+            <img className="gridPhoto" src={gridPhoto} alt=""></img>
+            <img className="gridPhoto" src={gridPhoto} alt=""></img>
+            <img className="gridPhoto" src={gridPhoto} alt=""></img>
+            <img className="gridPhoto" src={gridPhoto} alt=""></img>
             </div>
         </div>
     )
