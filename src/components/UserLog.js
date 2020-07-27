dfb00f4cc652168b31d8599b83cb760404ebf0de
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-27 17:57:50 -0400

    Log In/Log Out toggle working successfully

--- src/components/UserLog.js
+++ src/components/UserLog.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Conditional Rendering Source
+// https://stackoverflow.com/questions/40477245/is-it-possible-to-use-if-else-statement-in-react-render-function
+
 class UserLog extends React.Component {
     constructor () {
         super ()
@@ -18,15 +21,26 @@ class UserLog extends React.Component {
         })
     }
     render () {
+        const loggedIn = this.state.loggedIn;
         return (
             <div className="child userLog">
-                {
-                if (this.loggedIn == true) {
+                {(() => {
+                    if (loggedIn) {
+                    return (
+                        <button onClick={this.logOut}><h2>Log Out</h2></button>
+                    )
+                    } else {
+                    return (
+                        <button onClick={this.login}><h2>Log In</h2></button>
+                    )
+                    }
+                })()}
+                {/* if (loggedIn) {
                     <button onClick={this.logOut}><h2>Log Out</h2></button>
                 } else {
                     <button onClick={this.login}><h2>Log In</h2></button>
-                }
-            }
+                } */}
+            
             </div>
         )
     }
