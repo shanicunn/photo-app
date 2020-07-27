70779ba58f13d74d01759b0353c3769725b0e114
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-27 16:47:07 -0400

    Commented out home comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
-import Home from './components/Home';
+// import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Home />
+      {/* <Home /> */}
     </div>
   );
 }
