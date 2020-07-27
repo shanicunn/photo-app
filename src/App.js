6ffe72fb14bc8478d7ceedd062099d5407c76f60
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-27 16:53:22 -0400

    Commented out nav comp to put nav comp in home comp

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router,
 Switch, Route, Link } from 'react-router-dom';
-import Nav from './components/Nav';
-// import Home from './components/Home';
+// import Nav from './components/Nav';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      {/* <Home /> */}
+
+      {/* <Nav /> */}
+      <Home />
     </div>
   );
 }
