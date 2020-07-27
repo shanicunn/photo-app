7191f9ca03f864b0746b17dd16ccf8c34fc7d52c
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-27 17:07:10 -0400

    Added Profile to test

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { BrowserRouter as Router,
 Switch, Route, Link } from 'react-router-dom';
 // import Nav from './components/Nav';
-import Home from './components/Home';
+// import Home from './components/Home';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
 
       {/* <Nav /> */}
-      <Home />
+      {/* <Home /> */}
+      <Profile />
     </div>
   );
 }
